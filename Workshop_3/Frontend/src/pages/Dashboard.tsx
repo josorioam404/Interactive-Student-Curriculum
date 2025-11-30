6aafba488cc7a550902ede13cf8c4ae3f3733988
josorioam404 <josorioam404@gmail.com>
2025-11-30 10:39:05 -0500

    debug again

--- Workshop_3/Frontend/src/pages/Dashboard.tsx
+++ Workshop_3/Frontend/src/pages/Dashboard.tsx
@@ -29,6 +29,8 @@ export const Dashboard: React.FC = () => {
 
   const PYTHON_API_URL = import.meta.env.VITE_PYTHON_API_URL;
 
+  console.log('new print');
+
   console.log('Environment:', import.meta.env.MODE);
   console.log('Java API:', import.meta.env.VITE_JAVA_API_URL);
   console.log('Python API:', import.meta.env.VITE_PYTHON_API_URL);
