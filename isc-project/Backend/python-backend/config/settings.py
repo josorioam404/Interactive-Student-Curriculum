c1deacf5f8cc41144470058fb07dcf472cf3858f
josorioam404 <josorioam404@gmail.com>
2025-12-05 01:07:28 -0500

    CORS managed only for dev [PYTHON], Database docker-compose addapted

--- isc-project/Backend/python-backend/config/settings.py
+++ isc-project/Backend/python-backend/config/settings.py
@@ -1,16 +1,32 @@
+import os
 from pydantic import BaseModel
 from typing import List
-import os
+
 
 class Settings(BaseModel):
+    # App Info
     APP_TITLE: str = "Interactive Curriculum - Python Backend"
     APP_VERSION: str = "1.0.0"
-    
-    CORS_ORIGINS: List[str] = [
-        "http://localhost:5173",
-        "http://localhost:8080",
-        "http://54.89.37.108:5173",  
-        os.getenv("FRONTEND_URL", "http://54.89.37.108:5173")  
-    ]
+
+    # Environment: "dev" or "prod"
+    APP_ENV: str = os.getenv("APP_ENV", "prod")
+
+    # DATABASE (NO DEFAULT SECRETS)
+    DB_HOST: str = os.getenv("DB_HOST")
+    DB_NAME: str = os.getenv("DB_NAME")
+    DB_USER: str = os.getenv("DB_USER")
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD")
+    DB_PORT: str = os.getenv("DB_PORT")
+
+    # CORS (enabled only in dev)
+    @property
+    def CORS_ORIGINS(self) -> List[str]:
+        if self.APP_ENV == "dev":
+            return [
+                "http://localhost:5173",
+                "http://localhost:8080",
+            ]
+        return []
+
 
 settings = Settings()
